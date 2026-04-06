f5a1b7ef43c8a3eed6acaa5f74a94a50345f3610
Mariia Krasniuk <mariia.pirogova@gmail.com>
2026-04-06 15:22:23 +0300

    reverting back

--- transforms.py
+++ transforms.py
@@ -1,7 +1,7 @@
 from monai.transforms import (
     Compose, LoadImaged, EnsureChannelFirstd, Orientationd,
     Spacingd, NormalizeIntensityd, RandCropByPosNegLabeld,
-    RandFlipd, RandRotate90d, EnsureTyped
+    RandFlipd, EnsureTyped
 )
 
 def get_train_transforms():
@@ -16,10 +16,9 @@ def get_train_transforms():
             keys=["image", "label"],
             label_key="label",
             spatial_size=(96,96,96),
-            pos=1, neg=1, num_samples=6
+            pos=1, neg=1, num_samples=4
         ),
         RandFlipd(keys=["image", "label"], spatial_axis=0, prob=0.5),
-        RandRotate90d(keys=["image", "label"], prob=0.5, max_k=3),
         EnsureTyped(keys=["image", "label"]),
     ])
 
